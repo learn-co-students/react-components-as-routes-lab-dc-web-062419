97085699d186e24022ad982077dbbd50f0c0ac31
Harum Helmy <harumendhah@gmail.com>
2019-09-04 12:11:09 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {
+        actors.map( actor => {
+          return (
+            <div> 
+              <h2>Name: {actor.name} </h2>
+              Movies: {actor.movies.map( movie => 
+                <ul>
+                  <li>{movie}</li>
+                </ul>)}
+            </div>
+        )})
+      }
     </div>
   );
 };
